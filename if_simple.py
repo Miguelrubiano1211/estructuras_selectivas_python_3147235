f29868ff43fff83c5387c26c41a03faecee7ac4a
David <davidsmoraleslopez10@gmail.com>
2025-03-07 11:02:59 -0500

    funciones de condicionales

---
+++ if_simple.py
@@ -0,0 +1,14 @@
+'''
+sintaxis if simple
+if (condicion: 
+   instruccion1
+   intruccion2 
+
+'''
+
+#ejemplo:
+a = 9
+b = 3
+if a > b :
+    print ("a es myor que b")
+    print ("hoy es viernes y el cuerpo lo sabe")

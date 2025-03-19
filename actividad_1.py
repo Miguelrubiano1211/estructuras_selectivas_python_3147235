838521373b7f2ca1c2136bff3d751b4ce298cbac
JuanPabloBeltran <juanpablobeltranrodriguez@gmail.com>
2025-03-19 10:08:49 -0500

    primera actividad con if alse

---
+++ actividad_1.py
@@ -0,0 +1,18 @@
+'''temperatura=100
+
+if temperatura <= 80:
+    print ("el motor sigue encendido")
+else:
+    print ("el motor se aparara por sobre calentamiento")
+print("fin del proceso")
+'''
+
+
+
+
+temperatura= int(input("digite la temperatura: "))
+if temperatura <= 80:
+    print ("el motor sigue encendido")
+else:
+    print ("el motor se aparara por sobre calentamiento")
+print("fin del proceso")

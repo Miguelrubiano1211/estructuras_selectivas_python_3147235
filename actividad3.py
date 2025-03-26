933f0ec3933ddfcbcda9b11dfc6023650a414425
David <davidsmoraleslopez10@gmail.com>
2025-03-26 10:22:24 -0500

    actividad 3

--- actividad3.py
+++ actividad3.py
@@ -17,6 +17,43 @@ se debe solicitar:
 - numero de horas trabajadas y el valor apagar por hora
 - el total del salario se calcula de multiplicar el numero de horas por el valor a pagar por hora 
 
+
+=> para el caso de prestacion de servicio
+se debe solicitar: 
+- el valor de contrato 
+- el numero de meses de contrato 
+- la antiguedad del empleado (años)
+salario neto, en este caso se calcula:
+1. dividir el el valor del contrato por el numero de meses 
+2. restar el 15% del valor del contrato por consepto de eps
+3. restar el 10% del valor del contrato por concepto de pension 
+4. si el empleador tiene una antiguedad = o superior a 10 años:
+tiene una bonificacion del 0.5% sobre el salario mensual 
+
+
+
+=> Para el caso de contrato a termino indefinido 
+se debe solicitar:
+- Antiguedad (años)
+- Grado o escalafon (1-5)
+- salario minimo legal vigente 
+
+
+El salario mensual se calcula de acuerdo  a la siguiente tablaa:
+- grado 1: 1.5 SM
+- grado 2 : 1.7 SM
+- grado 3: 2.0 SM
+- grado 4: 2.5 SM
+- grado 5: 3.0 SM
+la bonificacion estara acorde a los siguientes rango segun la antiguedad:
+- entre 1 y 5 años : 1% el salario mensual
+- entre 6 y 10 años : 2% el salario mensual
+- superior a 10 años : al 3% del salario mensual
+- para este caso, los descuentos de ley son: 
+
+- 25% por concepto de la eps 
+- 22% por concepto de la pension
+- 0.1% por concepto de la arl
 '''
 
 contrato = input("ingrese el tipo de contrato: ")
@@ -25,8 +62,41 @@ contrato = input("ingrese el tipo de contrato: ")
 salario_neto = 0
 if contrato == "a":
     print("eligio: contrato termino indefinido.")
+    antiguedad = int(input("Ingrese la antiguedad del empleado (Año):"))
+    grado = int(input("Ingrese el grado del empleado:(1-5)"))
+    salario_neto= int(input("Ingrese el salario neto del empleado:"))
+    salario_minimo = int(input("Ingrese el salario minimo:"))
+    if grado == 1:
+        salario_neto = salario_minimo*1.5
+    elif grado == 2:
+        salario_neto = salario_minimo*1.7
+    elif grado == 3:
+        salario_neto = salario_minimo*2.0
+    elif grado == 4:
+        salario_neto = salario_minimo*2.5
+    elif grado == 5:
+        salario_neto = salario_minimo*3.0
+    if antiguedad >= 1 and antiguedad <= 5:
+        salario_neto = salario_neto + (salario_minimo*0.01)
+    elif antiguedad >= 6 and antiguedad <= 10:
+        salario_neto = salario_neto + (salario_minimo*0.02)
+    elif antiguedad >= 10:
+        salario_neto = salario_neto + (salario_minimo*0.03)
+    salario_neto = salario_neto - (salario_neto*0.25) - (salario_neto*0.22) - (salario_neto*0.001)
+
 elif contrato == "b":
     print ("eligio: contrato por prestacion de servicio.")
+    valor_contrato = int (input("ingrese el valor del contrato: "))
+    numero_meses = int (input("ingrese el numero de meses trabajador: "))
+    antiguedad = int(input("ingrese la antiguedad del empleado en años: "))
+    salario_mensual = valor_contrato / numero_meses 
+    EPS = salario_mensual * 0.15
+    pension = salario_mensual * 0.10 
+    bonificacion = salario_mensual * 0.05 
+    salario_neto = salario_mensual - EPS - pension
+    if antiguedad >= 10:
+        salario_neto = salario_mensual + bonificacion
+
 elif contrato == "c":
     print ("eligio: contrato de aprendizaje.")
     salario_minimo = int(input("ingrese el salario minimo legal vigente: "))
